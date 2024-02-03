128300b8089932f5cf39f918651f25916deceb59
Priyanka Khire <priyanka.khire@gmail.com>
2024-02-02 23:40:41 -0800

    Added Multithreading inside Multiprocessing

---
+++ Programs/Basics/Multithreading inside Multiprocessing.py
@@ -0,0 +1,42 @@
+# Multithreading inside Multiprocessing
+
+import threading
+import multiprocessing
+from multiprocessing import Process
+import time
+
+
+def task(processName, threadName):
+    print " Starting task for process ", processName, " and thread ", threadName
+    for i in range(3):
+        print " performing task for process ", processName, " and thread ", threadName
+        time.sleep(1)
+    print " Completed task for process ", processName, " and thread ", threadName
+
+def threadExecution(processName):
+    # Note the comma in args=(processName, "Thread1",)
+    thread1 = threading.Thread(target=task, args=(processName, "Thread1",))
+    thread2 = threading.Thread(target=task, args=(processName, "Thread2",))
+    
+    thread1.start()
+    thread2.start()
+    
+    thread1.join()
+    thread2.join()
+
+def processExecution():
+    # Note the comma in args=("Process1",)
+    process1 = Process(target=threadExecution, args=("Process1",))
+    process2 = Process(target=threadExecution, args=("Process2",))
+    
+    process1.start()
+    process2.start()
+    
+    process1.join()
+    process2.join()
+
+# Main
+if __name__ == '__main__': # this line for windows only
+    multiprocessing.freeze_support() # this line for windows only
+    # Start the process
+    processExecution()
--- Programs/README.md
+++ Programs/README.md
@@ -9,4 +9,7 @@ This program illustrates sequential execution where tasks are performed one afte
  
  - <a href="Basics/Multiprocessing.py">Multiprocessing</a></br>
   In this program, tasks A and B are executed concurrently using multiprocessing. Both tasks start simultaneously, and their execution overlaps across multiple processes, demonstrating concurrency at the process level.
+
+- <a href="Basics/Multithreading inside Multiprocessing.py">Multithreading inside Multiprocessing</a></br>
+  Shows multiple process and their multiple threads.
  
